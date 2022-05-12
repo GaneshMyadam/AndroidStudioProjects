52e8305f3f259f24194b2111e6a187f080b5988b
Ganesh Myadam <myadamganesh@gmail.com>
2022-05-12 12:56:12 +0530

    Update native-lib.cpp

--- NdkExample2/app/src/main/cpp/native-lib.cpp
+++ NdkExample2/app/src/main/cpp/native-lib.cpp
@@ -4,7 +4,7 @@ extern "C"
 JNIEXPORT jint JNICALL
 Java_com_example_ndkexample_MainActivity_getTextLen(
         JNIEnv* env,
-        jobject instance,
+        jobject,
         jstring txt_) {
  const char *txt = env->GetStringUTFChars(txt_,0);
 int len= strlen(txt);
