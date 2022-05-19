8eba0d7a68508b69dc9ba07a750730ec17861b8d
Ganesh Myadam <myadamganesh@gmail.com>
2022-05-19 11:17:50 +0530

    changed

--- NDKExample/jni/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := jniex
-LOCAL_SRC_FILE := jniex.c
-
-include $(BUILD_SHARED_LIBRARY)
--- NDKExample/jni/jniex.c
+++
@@ -1,9 +0,0 @@
-#include<jni.h>
-#include<string.h>
-
-jstring Java_com_example_ndkexample_MainActivity_HelloWorld
-(JNIEnv* env, jobject obj){
-
-(*env)->NewStringUTF(env," Helloworld from JNI");
-
-}
